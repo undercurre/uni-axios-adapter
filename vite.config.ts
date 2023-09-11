588ba6248f8d0a80e2bb5dce72557cfda3e63f66
undercurre <undercurre@163.com>
2023-09-12 02:01:19 +0800

    feat(release): 可构建

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'uni-adapter'
+    },
+    rollupOptions: {
+      output: {
+        format: 'esm'
+      }
+    }
+  }
 });
