3dc0393c307d162cc027f16a353aa315fa4155c3
lirh42 <undercurre@163.com>
2023-09-11 19:38:20 +0800

    feat(utils): 集成了lint-staged、husky、commitizen

---
+++ .cz-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	types: [
+		{ value: 'feat', name: '✨ 新功能 feat: 添加新功能' },
+		{ value: 'fix', name: '🐛 修复 bug fix: 修复bug' },
+		{ value: 'docs', name: '📚 文档 docs: 更新文档' },
+		{ value: 'style', name: '💄 格式 style: 代码格式化' },
+		// 添加更多类型...
+	],
+	scopes: [
+		{ name: 'function', description: '功能' },
+		{ name: 'utils', description: '工具' },
+		{ name: 'release', description: '版本' }
+		// 添加更多作用范围...
+	],
+	messages: {
+		type: '选择一种你的提交类型:',
+		scope: '选择一个scope (可选):',
+		customScope: '请输入自定义的scope:',
+		subject: '短说明:\n',
+		body: '长说明，使用"|"换行(可选):\n',
+		breaking: '非兼容性说明 (可选):\n',
+		footer: '关联关闭的issue, 例如：#31, #34(可选):\n',
+		confirmCommit: '确定提交说明?(yes/no)'
+	},
+	allowCustomScopes: true,
+	skipQuestions: ['body', 'footer'],
+	allowBreakingChanges: ['feat', 'fix']
+	// 其他配置...
+};
